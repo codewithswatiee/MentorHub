84e92d3757249fe912681d3d7a74db7a838dcccd
codewithswatiee <verma.s@somaiya.edu>
2024-10-21 01:59:55 +0530

    "Added Cloudinary configuration and updated .env file with new environment variables"

--- server/server.js
+++ server/server.js
@@ -1,3 +1,8 @@
 const express = require("express");
 const app = express();
 
+const userRoutes = require("./routes/User");
+const profileRoutes = require("./routes/Profile");
+const paymentRoutes = require("./routes/Payment");
+const courseRoutes = require("./routes/Course");
+
